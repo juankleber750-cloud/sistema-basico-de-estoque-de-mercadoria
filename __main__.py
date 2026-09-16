8f220848d20eb222185fc2bd9f03f99466988f63
juan_kleber <juankleber750@gmail.com>
2026-09-15 22:44:37 -0300

    função desc adicionada

--- __main__.py
+++ __main__.py
@@ -5,5 +5,5 @@ if __name__ == '__main__':
     banco1.criar_inventario()
     banco1.adicionar(nome='rapadura', valor='32.99')
     banco1.adicionar(nome='beijinho', valor='12.50')
-    print(banco1.ver_tabela(nome='*',ordem='nome', desc=False))
+    print(banco1.ver_tabela(nome='*',ordem='nome', desc=True))
     banco1.desligar()
